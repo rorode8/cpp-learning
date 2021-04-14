e3f7b84f6c806f8f7e3841d45b6651a9aa61e670
rorode8 <rorode8@hotmail.com>
2021-04-13 22:43:38 -0500

    enumerates

--- C++ primer/basics.cpp
+++ C++ primer/basics.cpp
@@ -1,12 +1,23 @@
 #include <cstdlib>
 #include <iostream>
 /* This program inputs two numbers x and y and outputs their sum */
+
+enum Day { SUN, MON, TUE, WED, THU, FRI, SAT };
+enum Mood { HAPPY = 3, SAD = 1, ANXIOUS = 4, SLEEPY = 2 };
+
 int main( ) {
   int x,y;
   std::cout << "Please enter two number";
   std::cin >> x >> y;
   int  s = x+y;
   std::cout << "Their sum is: " << s << std::endl;
+  Mood today = HAPPY;
+  Mood yesterday = SAD;
+  if(today > yesterday){
+    std::cout << yesterday << "\n";
+  }else{
+    std::cout << today << "\n";
+  }
   return EXIT_SUCCESS;
 
 }
